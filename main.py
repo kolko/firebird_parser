0340fd4b2eae3b9373a7195eda9cb37b4e825e7d
kolko <kolko@users.noreply.github.com>
2016-08-18 03:39:01 +0500

    Add check of correct parsing of fill bitmap in pointer page

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from db_file import DBReader
 
 def read_pages_table(db_reader):
     pages_address = db_reader.db_header.hdr_PAGES
-    print('RDB$PAGES address: {}'.format(pages_address))
     pages_table_page_list = []
     pages_table_page = db_reader.read_page(pages_address)
     pages_table_page_list.append(pages_table_page)
@@ -12,7 +11,6 @@ def read_pages_table(db_reader):
         # have more pages
         pages_table_page = db_reader.read_page(pages_table_page.ppg_next)
         pages_table_page_list.append(pages_table_page)
-    print('First page of RDB$PAGES: {}'.format(pages_table_page_list))
 
     for pages_table_page in pages_table_page_list:
         for page in pages_table_page.ppg_page:
@@ -21,12 +19,21 @@ def read_pages_table(db_reader):
                 continue
             data_page = db_reader.read_page(page['address'])
             assert data_page.header.pag_type == 5, 'RDB$PAGES must links to Data Pages'
+            # Check bit parser in bitmap
+            if data_page.header.pag_flags & 0b10:
+                assert page['full'] == 1
+            else:
+                assert page['full'] == 0
+            if data_page.header.pag_flags & 0b100:
+                assert page['has_large_obj'] == 1
+            else:
+                assert page['has_large_obj'] == 0
+
 
 
 def main():
     db_reader = DBReader('test_db.gdb')
     read_pages_table(db_reader)
-    # print(db_reader.read_page(819))
 
 
 if __name__ == '__main__':
