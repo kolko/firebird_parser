b9870a36e67f6b53953edc5b1a9761ed279bd1e7
kolko <kolko@users.noreply.github.com>
2016-08-19 23:26:12 +0500

    Start reading RDB. Broke some asserts :(

--- main.py
+++ main.py
@@ -53,12 +53,58 @@ def test_read_pages_table(db_reader):
     for page_row in pages_table:
         assert db_reader.read_page(page_row.p_page_number).header.pag_type == page_row.p_page_type
 
+def get_relationid_6(db_reader, pages_table):
+    #RDB$RELATIONS
+    pages = list(filter(lambda x: x.p_relation_id == 6, pages_table))
+    pages_pointer = list(filter(lambda x: x.p_page_type == 4, pages))
+    assert len(list(pages_pointer)) == 1
+    pointer_page = db_reader.read_page(pages_pointer[0].p_page_number)
+    for page in pointer_page.ppg_page:
+        if page['address'] == 0:
+            # Free page
+            continue
+        data_page = db_reader.read_page(page['address'])
+        assert data_page.header.pag_type == 5, '???? must links to Data Pages'
+        # Check bit parser in bitmap
+        # TODO: FIX
+        # if data_page.header.pag_flags & 0b10:
+        #     assert page['full'] == 1
+        # else:
+        #     assert page['full'] == 0
+        # if data_page.header.pag_flags & 0b100:
+        #     assert page['has_large_obj'] == 1
+        # else:
+        #     assert page['has_large_obj'] == 0
+        for pages_row in data_page.dpg_rpt:
+            # RDB$VIEW_BLR                    <null>
+            # RDB$VIEW_SOURCE                 <null>
+            # RDB$DESCRIPTION                 <null>
+            # RDB$RELATION_ID                 6
+            # RDB$SYSTEM_FLAG                 1
+            # RDB$DBKEY_LENGTH                8
+            # RDB$FORMAT                      0
+            # RDB$FIELD_ID                    17
+            # RDB$RELATION_NAME               RDB$RELATIONS
+            # RDB$SECURITY_CLASS              <null>
+            # RDB$EXTERNAL_FILE               <null>
+            # RDB$RUNTIME                     6:32
+            # BLOB display set to subtype 1. This BLOB: subtype = 5
+            # RDB$EXTERNAL_DESCRIPTION        <null>
+            # RDB$OWNER_NAME                  SYSDBA
+            # RDB$DEFAULT_CLASS               <null>
+            # RDB$FLAGS                       <null>
+            # RDB$RELATION_TYPE               0
+
+            data = pages_row.data_uncompressed
+            print(data)
 def main():
     db_reader = DBReader('test_db.gdb')
     test_read_pages_table(db_reader)
     pages_table = read_pages_table(db_reader)
-    print([x.__dict__ for x in pages_table])
+    get_relationid_6(db_reader, pages_table)
 
+
+# const int REQ_relation_id		= 2;
 
 if __name__ == '__main__':
     main()
