bc7f285418b4aa97b57a99440d667507f31cf525
kolko <kolko@users.noreply.github.com>
2016-08-23 23:02:33 +0500

    Try parse some new system tables...

--- main.py
+++ main.py
@@ -55,6 +55,17 @@ def test_read_pages_table(db_reader):
 
 def get_relationid_6(db_reader, pages_table):
     #RDB$RELATIONS
+    class RelationsTableRow(object):
+        def __init__(self, p_relation_id, p_system_flag, p_dbkey_length, p_format, p_field_id,
+                     p_relation_name, p_security_class):
+            self.p_relation_id = p_relation_id
+            self.p_system_flag = p_system_flag
+            self.p_dbkey_length = p_dbkey_length
+            self.p_format = p_format
+            self.p_field_id = p_field_id
+            self.p_relation_name = p_relation_name
+            self.p_security_class = p_security_class
+    result = []
     pages = list(filter(lambda x: x.p_relation_id == 6, pages_table))
     pages_pointer = list(filter(lambda x: x.p_page_type == 4, pages))
     assert len(list(pages_pointer)) == 1
@@ -76,35 +87,135 @@ def get_relationid_6(db_reader, pages_table):
         # else:
         #     assert page['has_large_obj'] == 0
         for pages_row in data_page.dpg_rpt:
-            # RDB$VIEW_BLR                    <null>
-            # RDB$VIEW_SOURCE                 <null>
-            # RDB$DESCRIPTION                 <null>
-            # RDB$RELATION_ID                 6
-            # RDB$SYSTEM_FLAG                 1
-            # RDB$DBKEY_LENGTH                8
-            # RDB$FORMAT                      0
-            # RDB$FIELD_ID                    17
-            # RDB$RELATION_NAME               RDB$RELATIONS
-            # RDB$SECURITY_CLASS              <null>
-            # RDB$EXTERNAL_FILE               <null>
-            # RDB$RUNTIME                     6:32
-            # BLOB display set to subtype 1. This BLOB: subtype = 5
-            # RDB$EXTERNAL_DESCRIPTION        <null>
-            # RDB$OWNER_NAME                  SYSDBA
-            # RDB$DEFAULT_CLASS               <null>
-            # RDB$FLAGS                       <null>
-            # RDB$RELATION_TYPE               0
-
             data = pages_row.data_uncompressed
-            print(data)
+            #TODO: remove hack
+            if len(data) < 100:
+                continue
+            p_relation_id, p_system_flag, p_dbkey_length, p_format, p_field_id = struct.unpack_from('<HHHHH', data, offset=28)
+            p_relation_name = ''.join(x.decode(db_reader.charset) for x in struct.unpack_from('<'+('c'*31), data, offset=38)).strip()
+            p_security_class = ''.join(x.decode(db_reader.charset) for x in struct.unpack_from('<'+('c'*31), data, offset=69)).strip()
+
+            # TODO: other fields
+            result.append(RelationsTableRow(p_relation_id, p_system_flag, p_dbkey_length, p_format, p_field_id, p_relation_name, p_security_class))
+    return result
+
+def get_database_table_relid_1(db_reader, pages_table):
+    # RDB$DESCRIPTION                 (RDB$DESCRIPTION) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$RELATION_ID                 (RDB$RELATION_ID) SMALLINT Nullable
+    # RDB$SECURITY_CLASS              (RDB$SECURITY_CLASS) CHAR(31) CHARACTER SET UNICODE_FSS Nullable
+    # RDB$CHARACTER_SET_NAME          (RDB$CHARACTER_SET_NAME) CHAR(31) CHARACTER SET UNICODE_FSS Nullable
+    pages = list(filter(lambda x: x.p_relation_id == 1, pages_table))
+    pages_pointer = list(filter(lambda x: x.p_page_type == 4, pages))
+    assert len(list(pages_pointer)) == 1
+    pointer_page = db_reader.read_page(pages_pointer[0].p_page_number)
+    for page in pointer_page.ppg_page:
+        if page['address'] == 0:
+            # Free page
+            continue
+        data_page = db_reader.read_page(page['address'])
+        assert data_page.header.pag_type == 5, '???? must links to Data Pages'
+        for pages_row in data_page.dpg_rpt:
+            data = pages_row.data_uncompressed
+            # TODO parse blob with description
+            p_relation_id, = struct.unpack_from('<H', data, offset=12)
+            p_security_class = ''.join(x.decode('utf8') for x in struct.unpack_from('<'+('c'*31), data, offset=14)).strip()
+            p_character_set_name = ''.join(x.decode('utf8') for x in struct.unpack_from('<'+('c'*31), data, offset=45)).strip()
+
+            if p_character_set_name.lower() == 'win1251':
+                p_character_set_name = 'cp1251'
+            return p_character_set_name.lower()
+
+
+def formats_table(db_reader, pages_table, relations):
+    # RDB$RELATION_ID                 (RDB$RELATION_ID) SMALLINT Nullable
+    # RDB$FORMAT                      (RDB$FORMAT) SMALLINT Nullable
+    # RDB$DESCRIPTOR                  (RDB$DESCRIPTOR) BLOB segment 80, subtype FORMAT CHARACTER SET NONE Nullable
+    relation = list(filter(lambda x: x.p_relation_name == 'RDB$FORMATS', relations))
+    assert len(relation) == 1
+    pages = list(filter(lambda x: x.p_relation_id == relation[0].p_relation_id, pages_table))
+    pages_pointer = list(filter(lambda x: x.p_page_type == 4, pages))
+    assert len(list(pages_pointer)) == 1
+    pointer_page = db_reader.read_page(pages_pointer[0].p_page_number)
+    for page in pointer_page.ppg_page:
+        if page['address'] == 0:
+            # Free page
+            continue
+        data_page = db_reader.read_page(page['address'])
+        assert data_page.header.pag_type == 5, '???? must links to Data Pages'
+        for pages_row in data_page.dpg_rpt:
+            data = pages_row.data_uncompressed
+            # TODO: remove hack
+            if not data:
+                continue
+            p_relation_id, p_format = struct.unpack_from('<HH', data, offset=5)
+            print(p_relation_id, p_format)
+            print(data[9:])
+
+
+def fields_table(db_reader, pages_table, relations):
+    # RDB$FIELD_NAME                  (RDB$FIELD_NAME) CHAR(31) CHARACTER SET UNICODE_FSS Nullable
+    # RDB$QUERY_NAME                  (RDB$FIELD_NAME) CHAR(31) CHARACTER SET UNICODE_FSS Nullable
+    # RDB$VALIDATION_BLR              (RDB$VALIDATION_BLR) BLOB segment 80, subtype BLR CHARACTER SET NONE Nullable
+    # RDB$VALIDATION_SOURCE           (RDB$SOURCE) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$COMPUTED_BLR                (RDB$VALUE) BLOB segment 80, subtype BLR CHARACTER SET NONE Nullable
+    # RDB$COMPUTED_SOURCE             (RDB$SOURCE) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$DEFAULT_VALUE               (RDB$VALUE) BLOB segment 80, subtype BLR CHARACTER SET NONE Nullable
+    # RDB$DEFAULT_SOURCE              (RDB$SOURCE) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$FIELD_LENGTH                (RDB$FIELD_LENGTH) SMALLINT Nullable
+    # RDB$FIELD_SCALE                 (RDB$FIELD_SCALE) SMALLINT Nullable
+    # RDB$FIELD_TYPE                  (RDB$FIELD_TYPE) SMALLINT Nullable
+    # RDB$FIELD_SUB_TYPE              (RDB$FIELD_SUB_TYPE) SMALLINT Nullable
+    # RDB$MISSING_VALUE               (RDB$VALUE) BLOB segment 80, subtype BLR CHARACTER SET NONE Nullable
+    # RDB$MISSING_SOURCE              (RDB$SOURCE) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$DESCRIPTION                 (RDB$DESCRIPTION) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$SYSTEM_FLAG                 (RDB$SYSTEM_FLAG) SMALLINT Nullable
+    # RDB$QUERY_HEADER                (RDB$QUERY_HEADER) BLOB segment 80, subtype TEXT CHARACTER SET UNICODE_FSS Nullable
+    # RDB$SEGMENT_LENGTH              (RDB$SEGMENT_LENGTH) SMALLINT Nullable
+    # RDB$EDIT_STRING                 (RDB$EDIT_STRING) VARCHAR(125) CHARACTER SET NONE Nullable
+    # RDB$EXTERNAL_LENGTH             (RDB$FIELD_LENGTH) SMALLINT Nullable
+    # RDB$EXTERNAL_SCALE              (RDB$FIELD_SCALE) SMALLINT Nullable
+    # RDB$EXTERNAL_TYPE               (RDB$FIELD_TYPE) SMALLINT Nullable
+    # RDB$DIMENSIONS                  (RDB$DIMENSIONS) SMALLINT Nullable
+    # RDB$NULL_FLAG                   (RDB$NULL_FLAG) SMALLINT Nullable
+    # RDB$CHARACTER_LENGTH            (RDB$FIELD_LENGTH) SMALLINT Nullable
+    # RDB$COLLATION_ID                (RDB$COLLATION_ID) SMALLINT Nullable
+    # RDB$CHARACTER_SET_ID            (RDB$CHARACTER_SET_ID) SMALLINT Nullable
+    # RDB$FIELD_PRECISION             (RDB$FIELD_PRECISION) SMALLINT Nullable
+    relation = list(filter(lambda x: x.p_relation_name == 'RDB$FIELDS', relations))
+    assert len(relation) == 1
+    pages = list(filter(lambda x: x.p_relation_id == relation[0].p_relation_id, pages_table))
+    pages_pointer = list(filter(lambda x: x.p_page_type == 4, pages))
+    assert len(list(pages_pointer)) == 1
+    pointer_page = db_reader.read_page(pages_pointer[0].p_page_number)
+    for page in pointer_page.ppg_page:
+        if page['address'] == 0:
+            # Free page
+            continue
+        data_page = db_reader.read_page(page['address'])
+        assert data_page.header.pag_type == 5, '???? must links to Data Pages'
+        for pages_row in data_page.dpg_rpt:
+            data = pages_row.data_uncompressed
+            # TODO: remove hack
+            if not data:
+                continue
+            # TODO: parse all
+            p_field_name = ''.join(x.decode(db_reader.charset) for x in struct.unpack_from('<'+('c'*31), data, offset=4)).strip()
+            p_query_name = ''.join(x.decode(db_reader.charset) for x in struct.unpack_from('<'+('c'*31), data, offset=35)).strip()
+            print(p_field_name, p_query_name)
+            # print(data)
+
+
 def main():
     db_reader = DBReader('test_db.gdb')
     test_read_pages_table(db_reader)
     pages_table = read_pages_table(db_reader)
-    get_relationid_6(db_reader, pages_table)
+    db_charset = get_database_table_relid_1(db_reader, pages_table)
+    db_reader.charset = db_charset
+    relations = get_relationid_6(db_reader, pages_table)
+    # formats_table(db_reader, pages_table, relations)
+    fields_table(db_reader, pages_table, relations)
+    # print(relations)
 
-
-# const int REQ_relation_id		= 2;
 
 if __name__ == '__main__':
     main()
